c94cfa37c10f1502f2d1e89929862c8a8bc43913
Nick Hilton <weegreenblobbie2@gmail.com>
2025-03-25 20:35:51 -0700

    improved moon image generation

--- nss/utils.py
+++ nss/utils.py
@@ -4,13 +4,20 @@ import matplotlib.pyplot as plt
 from scipy.signal import convolve2d
 
 
-def _gaus2d(x=0, y=0, mx=0, my=0, sx=1, sy=1):
+def _gaus2d(x=0.0, y=0.0, mx=0.0, my=0.0, sx=1.0, sy=1.0):
     return np.exp(-((x - mx)**2.0 / (2.0 * sx**2.0) + (y - my)**2.0 / (2.0 * sy**2.0)))
+
+
+def _make_2d_gaussian(size):
+    x = np.linspace(-size, size, 2 * size + 1)
+    x, y = np.meshgrid(x, x)
+    g = _gaus2d(x, y)
+    g /= np.sum(g.flatten())
+    return g
 
 
 def _generate_non_overlapping_creators(outer_radius, num_circles, min_radius, max_radius):
     circles = []
-    
     while len(circles) < num_circles:
         # Generate random center and radius.
         radius = random.uniform(min_radius, max_radius)
@@ -26,25 +33,38 @@ def _generate_non_overlapping_creators(outer_radius, num_circles, min_radius, ma
             if distance <= radius + cr:
                 overlap = True
                 break
-        
         # Add circle if no overlap
         if not overlap:
             circles.append((x, y, radius))
-            
     return circles
 
 
-def _write_circle(array, center, radius, value, assign=False):
+def _write_circle(array, center, radius, value):
     M, N = array.shape
     m_center, n_center = center
-    for m in range(m_center - radius, min(m_center + radius, M)):
-            for n in range(n_center - radius, min(n_center + radius, N)):
-                dist = np.hypot(m - m_center, n - n_center)
-                if dist <= radius:
-                    if assign:
-                        array[m,n] = value
-                    else:
-                        array[m,n] += value
+    for m in range(max(0, m_center - radius), min(m_center + radius, M)):
+        for n in range(max(0, n_center - radius), min(n_center + radius, N)):
+            dist = np.hypot(m - m_center, n - n_center)
+            if dist <= radius:
+                array[m,n] += value
+
+
+def generate_moon(radius, num_creators=100):
+    """
+    Generates an array radius x radius with the specified number of creators.
+    """
+    out = np.zeros((2*radius,2*radius), dtype=np.float32)
+    _write_circle(out, (radius, radius), radius, 0.250)
+    # Add random creators.
+    for creator in _generate_non_overlapping_creators(radius, num_creators, 5, radius//3):
+        mc, nc, r = creator
+        mc = int(mc + radius)
+        nc = int(nc + radius)
+        r = int(r)
+        _write_circle(out, (mc, nc), r, np.random.normal(0.250, 0.100))
+    out /= np.max(out.flatten())
+    print(f"max(moon) = {np.max(out.flatten())}")
+    return out
 
 
 def generate_test_image(image_shape=(1600,2400), num_stars_min=16, num_stars_max=32, radius_moon=None, num_moon_creators=None):
@@ -59,42 +79,38 @@ def generate_test_image(image_shape=(1600,2400), num_stars_min=16, num_stars_max
 
     M, N = image_shape
 
-    print(image_shape)
-
     out = np.zeros(image_shape, dtype=np.float32)
     num_stars = np.random.randint(num_stars_min, num_stars_max)
 
+    if radius_moon is not None:
+        assert radius_moon >= 100
+        m0 = np.random.randint(radius_moon, M - 2 * radius_moon)
+        n0 = np.random.randint(radius_moon, N - 2 * radius_moon)
+        moon = generate_moon(radius_moon, 100)
+        m1 = m0 + moon.shape[0]
+        n1 = n0 + moon.shape[1]
+        if m1 > M:
+            m1 = M
+        if n1 > N:
+            n1 = N
+        out[m0:m1, n0:n1] = moon[0:m1-m0, 0:n1-n0]
+
+    # Blur with a gaussian.
+    size = np.random.randint(1, 3)
+    g = _make_2d_gaussian(size)
+    
     for _ in range(num_stars):
         m = np.random.randint(0, M)
         n = np.random.randint(0, N)
-        out[m,n] = 1.0
+        out[m,n] = 4.0
 
-    # Create gaussian kernel.
-    kernel_size = np.random.randint(1, 3)
-    x = np.linspace(-kernel_size, kernel_size, 2 * kernel_size + 1)
-    x, y = np.meshgrid(x, x)
-    g = _gaus2d(x, y)
+    out = convolve2d(out, g, mode="same", boundary="symm")
 
-    out = convolve2d(out, g, mode='full')
-
-    if radius_moon is not None:
-        assert radius_moon >= 100
-
-        # Moon center.
-        m_center = np.random.randint(radius_moon, M - radius_moon)
-        n_center = np.random.randint(radius_moon, N - radius_moon)
-        _write_circle(out, (m_center, n_center), radius_moon, 0.500, assign=True)
-
-        # Add random creators.
-        for creator in _generate_non_overlapping_creators(radius_moon, 100, 5, radius_moon//4):
-            mc, nc, r = creator
-            mc = int(mc)
-            nc = int(nc)
-            r = int(r)
-            center = (m_center + mc, n_center + nc)
-            _write_circle(out, center, r, np.random.normal(0.500, 0.300))
-
-    return out
+    # Add some noise.
+    out += np.random.uniform(0.0, 0.15, (M,N))
+    out /= np.max(out.flatten())
+    out[out <= 0.0] = 0.0
+    return (50_000 * out).astype(np.uint16)
 
 
 def imshow(array):
@@ -102,18 +118,13 @@ def imshow(array):
     Plots a 2d array to the current maptloblib axes.
     """
 
-    peak = np.nanmax(array)
-
-    if peak < 1.0:
-        peak = 1.0
-
     # Grayscale.
     if array.ndim == 2:
-        plt.imshow(array / peak, cmap = 'gray', interpolation='nearest')
+        plt.imshow(array, cmap = 'gray', interpolation='nearest')
 
     # Color image.
     elif array.ndim == 3:
-        plt.imshow(array / peak, interpolation='nearest')
+        plt.imshow(array, interpolation='nearest')
 
     else:
         raise RuntimeError("don't know how to handle array with shape %s" % repr(array.shape))
